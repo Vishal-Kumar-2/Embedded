554268a2113f8170e9cf2ddbae2f6c608211c5f9
Vishal Kumar <vishalweb750@gmail.com>
2019-02-08 20:33:19 +0530

    Removed save image queue

--- backend/lib/cron.js
+++ backend/lib/cron.js
@@ -3,7 +3,6 @@ import Queue from 'bull';
 import { getHotstreaks } from '../app/services/hotStreak';
 
 let HotstreakQueue = new Queue('hot_streak_queue', config.REDIS_CONFIG);
-let SaveLocationImageQueue = new Queue('save_location_image_queue', config.REDIS_CONFIG);
 
 const startCron = () => {
   HotstreakQueue.process('hotstreaks',(job, done) => {
