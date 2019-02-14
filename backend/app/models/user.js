ac959238fda63a83f1072860902ca9c50656a3d6
Vishal Kumar <vishalweb750@gmail.com>
2019-02-14 14:25:11 +0530

    Added email,phone and password validation

--- backend/app/models/user.js
+++ backend/app/models/user.js
@@ -1,17 +1,48 @@
 import mongoose from 'mongoose';
 let Schema = mongoose.Schema;
 import { createHash } from '../../lib/bcrypt';
+const regexFormat = {
+  email: /^[_a-zA-Z0-9]+(\.[_a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$/,
+  password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/,
+  phone: /\d{3}-\d{3}-\d{4}/
+}
 
 const userSchema = new Schema({
   username: { type: String , required : true },
-  password: { type: String , required : true },
+  password: { 
+    type: String , 
+    required : true,
+    validate: {
+      validator: function(v) {
+        return regexFormat.password.test(v);
+      },
+      message: props => `Password must contain a capital letter, a number and a special character. Minimum length is eight characters and maximum length is sixteen.`
+    },
+  },
   id: Schema.Types.ObjectId,
-  email: { type: String , required : true },
+  email: { 
+    type: String , 
+    required : true,
+    validate: {
+      validator: function(v) {
+        return regexFormat.email.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
+    },
+  },
   details: {
     firstName: String,
     lastName: String,
     company: String,
-    phone: String,
+    phone: { 
+      type: String , 
+      validate: {
+        validator: function(v) {
+          return regexFormat.phone.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number`
+      },
+    },
   },
   locale: String,
   accountStatus: String,
